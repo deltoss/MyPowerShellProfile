050db111adee32c6eae7aff873902a42b37f2d5f
EmTee <mtran0011@outlook.com>
2025-09-11 11:52:15 +1000

    Added Visual Studio IDE as an opener

--- environments.ps1
+++ environments.ps1
@@ -17,3 +17,19 @@ function Set-EnvVarIfNotSet {
 Set-EnvVarIfNotSet -Name 'XDG_CONFIG_HOME' -Value "$env:USERPROFILE/.config"
 Set-EnvVarIfNotSet -Name 'EDITOR' -Value "nvim"
 
+# Adds devenv to the path, so you can use:
+#   devenv MySolution.sln
+#   devenv MySolution.sln /build "Debug|Any CPU"
+#   devenv MySolution.sln /rebuild "Release|x64"
+#   devenv MySolution.sln /clean
+$vsPath = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath
+if ($vsPath) {
+    $devenvPath = Join-Path $vsPath "Common7\IDE"
+
+    $env:PATH += ";$devenvPath"
+
+    Write-Host "Added to PATH: $devenvPath"
+    Write-Host "Visual Studio version found: $(Split-Path (Split-Path $vsPath) -Leaf)"
+} else {
+    Write-Host "No Visual Studio installation found"
+}
