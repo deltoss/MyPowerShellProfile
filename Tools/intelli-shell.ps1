56a22cfa8c12faa63bf1c4f3937d7cc47c511725
EmTee <mtran0011@outlook.com>
2025-10-27 17:39:01 +1100

    Added ability to import commands

--- Tools/intelli-shell.ps1
+++ Tools/intelli-shell.ps1
@@ -1,5 +1,7 @@
 $env:INTELLI_HOME = "$env:USERPROFIlE/intelli-shell/data"
 $env:INTELLI_CONFIG = "$env:XDG_CONFIG_HOME/intelli-shell/config.toml"
+$env:INTELLI_COMMANDS = "$env:XDG_CONFIG_HOME/intelli-shell/commands/"
 $env:INTELLI_SEARCH_HOTKEY = "Ctrl+e"
 
 intelli-shell.exe init pwsh | Out-String | Invoke-Expression
+Get-ChildItem -Path $env:INTELLI_COMMANDS | ForEach-Object { & intelli-shell import $_.FullName }
