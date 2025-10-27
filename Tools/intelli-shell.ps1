581471542bd2ccb7a77ab927e73f427a1e1f2151
EmTee <mtran0011@outlook.com>
2025-10-27 19:40:56 +1100

    Removed import functionalizy of intelli-shell, as it's too slow

--- Tools/intelli-shell.ps1
+++ Tools/intelli-shell.ps1
@@ -4,4 +4,3 @@ $env:INTELLI_COMMANDS = "$env:XDG_CONFIG_HOME/intelli-shell/commands/"
 $env:INTELLI_SEARCH_HOTKEY = "Ctrl+e"
 
 intelli-shell.exe init powershell | Out-String | Invoke-Expression
-Get-ChildItem -Path $env:INTELLI_COMMANDS | ForEach-Object { & intelli-shell import $_.FullName }
