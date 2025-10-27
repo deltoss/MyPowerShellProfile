e167e3eb0bee25a2b5653cab1346e92487cac414
EmTee <mtran0011@outlook.com>
2025-10-27 18:06:02 +1100

    Adjusted to follow the docs

--- Tools/intelli-shell.ps1
+++ Tools/intelli-shell.ps1
@@ -3,5 +3,5 @@ $env:INTELLI_CONFIG = "$env:XDG_CONFIG_HOME/intelli-shell/config.toml"
 $env:INTELLI_COMMANDS = "$env:XDG_CONFIG_HOME/intelli-shell/commands/"
 $env:INTELLI_SEARCH_HOTKEY = "Ctrl+e"
 
-intelli-shell.exe init pwsh | Out-String | Invoke-Expression
+intelli-shell.exe init powershell | Out-String | Invoke-Expression
 Get-ChildItem -Path $env:INTELLI_COMMANDS | ForEach-Object { & intelli-shell import $_.FullName }
