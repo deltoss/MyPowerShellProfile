9d570a3710bc2a4aa166fbb19906f28ea8bf7f18
EmTee <mtran0011@outlook.com>
2025-11-03 15:53:56 +1100

    Improved Eza integration

--- Tools/eza.ps1
+++ Tools/eza.ps1
@@ -1,4 +1,12 @@
 function Invoke-Eza {
-    & eza --oneline -A @args
+  $expandedArgs = $args | ForEach-Object {
+    if ($_ -match '^~') {
+      $_ -replace '^~', $HOME
+    } else {
+      $_
+    }
+  }
+  & eza --oneline -A $expandedArgs
 }
+
 Set-Alias -Name ls -Value Invoke-Eza
