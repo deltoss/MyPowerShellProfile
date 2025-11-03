b5a156257eb1267d3ff16d7c93781f33ef25beca
m.tran <m.tran@leapdev.io>
2025-11-03 13:59:01 +1100

    Removed problematic code that causes issues within visual studio or others terminals

--- Tools/starship.ps1
+++ Tools/starship.ps1
@@ -6,35 +6,12 @@ Invoke-Expression (&starship init powershell)
 #   https://wezterm.org/shell-integration.html#osc-7-on-windows-with-powershell-with-starship
 $prompt = ""
 function Invoke-Starship-PreCommand {
-  $isVisualStudio = $env:VisualStudioVersion -or $env:VSAPPIDNAME -or $env:VSLANG
-  if (-not $isVisualStudio) {
-    $current_location = $executionContext.SessionState.Path.CurrentLocation
-    if ($current_location.Provider.Name -eq "FileSystem") {
-      $ansi_escape = [char]27
-      $provider_path = $current_location.ProviderPath -replace "\\", "/"
-      $prompt = "$ansi_escape]7;file://${env:COMPUTERNAME}/${provider_path}$ansi_escape\"
+  $current_location = $executionContext.SessionState.Path.CurrentLocation
+  if ($current_location.Provider.Name -eq "FileSystem") {
+    $ansi_escape = [char]27
+    $provider_path = $current_location.ProviderPath -replace "\\", "/"
+    $prompt = "$ansi_escape]7;file://${env:COMPUTERNAME}/${provider_path}$ansi_escape\"
 
-      # See:
-      #   https://www.reddit.com/r/wezterm/comments/1fztaj8/comment/lr97ry9/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
-      $title = GetCurrentDir($current_location)
-      $title = "📂 " + $title
-
-      wezterm cli set-tab-title $title;
-      $host.ui.Write($prompt)
-    }
+    $host.ui.Write($prompt)
   }
-}
-
-function GetCurrentDir {
-  param ([string]$path = "")
-
-  if ($path -eq "") {
-    $path = Get-Location
-  }
-
-  if ($path -eq "$env:USERPROFILE") {
-    return "~"
-  }
-
-  return Split-Path ($path) -Leaf 
 }
