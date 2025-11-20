ed06cd2e93593c2bab7cbb2f31d1d1a1dd41ac6b
m.tran <m.tran@leapdev.io>
2025-11-20 13:55:32 +1100

    Fixed yazi integration with other tools such as neovim

--- Tools/yazi.ps1
+++ Tools/yazi.ps1
@@ -2,28 +2,33 @@
 # See:
 #   https://yazi-rs.github.io/docs/installation#windows
 $env:YAZI_FILE_ONE = "C:\Program Files\Git\usr\bin\file.exe"
-$env:YAZI_CONFIG_HOME="$env:XDG_CONFIG_HOME/yazi"
+
+Set-EnvVarIfNotSet -Name 'YAZI_CONFIG_HOME' -Value "$env:XDG_CONFIG_HOME/yazi"
 
 # Yazi File Manager
 # Based on:
 #   https://yazi-rs.github.io/docs/quick-start#shell-wrapper
-function y {
-    $tmp = (New-TemporaryFile).FullName
-    yazi $args --cwd-file="$tmp"
-    $cwd = Get-Content -Path $tmp -Encoding UTF8
-    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
-        Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
-    }
-    Remove-Item -Path $tmp
+function y
+{
+  $tmp = (New-TemporaryFile).FullName
+  yazi $args --cwd-file="$tmp"
+  $cwd = Get-Content -Path $tmp -Encoding UTF8
+  if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path)
+  {
+    Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+  }
+  Remove-Item -Path $tmp
 }
 
-function Get-PathWithYazi {
-    $tmp = (New-TemporaryFile).FullName
-    yazi $args --cwd-file="$tmp"
-    $cwd = Get-Content -Path $tmp -Encoding UTF8
-    Remove-Item -Path $tmp
-    if (-not [String]::IsNullOrEmpty($cwd)) {
-        return (Resolve-Path -LiteralPath $cwd).Path
-    }
-    return $null
+function Get-PathWithYazi
+{
+  $tmp = (New-TemporaryFile).FullName
+  yazi $args --cwd-file="$tmp"
+  $cwd = Get-Content -Path $tmp -Encoding UTF8
+  Remove-Item -Path $tmp
+  if (-not [String]::IsNullOrEmpty($cwd))
+  {
+    return (Resolve-Path -LiteralPath $cwd).Path
+  }
+  return $null
 }
