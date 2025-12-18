d95f0edb22d95de6c12ee58105acb526825199fe
m.tran <m.tran@leapdev.io>
2025-12-18 11:06:29 +1100

    Removed problematic code that conflicts with intelli-shell

--- environments.ps1
+++ environments.ps1
@@ -1,4 +1,5 @@
-function Set-EnvVarIfNotSet {
+function Set-EnvVarIfNotSet
+{
   param(
     [Parameter(Mandatory = $true)]
     [string]$Name,
@@ -7,7 +8,8 @@ function Set-EnvVarIfNotSet {
     [string]$Value
   )
 
-  if (-not (Get-Item "env:$Name" -ErrorAction SilentlyContinue)) {
+  if (-not (Get-Item "env:$Name" -ErrorAction SilentlyContinue))
+  {
     Set-Item -Path "env:$Name" -Value $Value
     [System.Environment]::SetEnvironmentVariable($Name, $Value, 'User')
     Write-Host "Set $Name to: $Value" -ForegroundColor Green
@@ -23,13 +25,15 @@ Set-EnvVarIfNotSet -Name 'EDITOR' -Value "nvim"
 #   devenv MySolution.sln /rebuild "Release|x64"
 #   devenv MySolution.sln /clean
 $vsPath = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -property installationPath
-if ($vsPath) {
+if ($vsPath)
+{
   $devenvPath = Join-Path $vsPath "Common7\IDE"
 
   $env:PATH += ";$devenvPath"
 
-  Write-Host "Added to PATH: $devenvPath" -ForegroundColor Green
-  Write-Host "Visual Studio version found: $(Split-Path (Split-Path $vsPath) -Leaf)" -ForegroundColor Green
-} else {
-  Write-Host "No Visual Studio installation found" -ForegroundColor Yellow
+  Write-Debug "Added to PATH: $devenvPath"
+  Write-Debug "Visual Studio version found: $(Split-Path (Split-Path $vsPath) -Leaf)"
+} else
+{
+  Write-Debug "No Visual Studio installation found"
 }
