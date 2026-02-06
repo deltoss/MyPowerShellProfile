cb18dd6ccd18188343a10a0454913482f27fbbc7
EmTee <mtran0011@outlook.com>
2026-02-06 18:23:55 +1100

    Factored out environments.ps1

--- profile.ps1
+++ profile.ps1
@@ -3,8 +3,6 @@ $scriptDir = $PSScriptRoot
 Import-Module "$scriptDir/Modules/WhichMenu"
 Import-Module "$scriptDir/Modules/RandomCliTip"
 
-. "$scriptDir/environments.ps1"
-
 Get-ChildItem -Path "$scriptDir/Tools/*.ps1" | ForEach-Object { . $_.FullName }
 
 Get-ChildItem -Path "$scriptDir/Menus/*.ps1" | ForEach-Object { . $_.FullName }
