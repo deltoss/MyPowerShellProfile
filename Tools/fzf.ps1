7573d0901d8a3ef38369b90d0bfaedcd8cb21d26
EmTee <mtran0011@outlook.com>
2026-02-07 19:19:24 +1100

    Refactored fzf & zoxide

--- Tools/fzf.ps1
+++ Tools/fzf.ps1
@@ -1,3 +1,3 @@
-$env:FZF_DEFAULT_COMMAND = 'es count:100' # Use Everything CLI by default
+$env:FZF_DEFAULT_COMMAND = 'fd --type f'
 $env:FZF_DEFAULT_OPTS = '--height 40% --layout=reverse --border --with-shell "pwsh -NoLogo -NonInteractive -NoProfile -ExecutionPolicy Bypass -Command"'
 $env:FZF_CUSTOM_PREVIEW = 'if (Test-Path -Path {} -PathType Container) { eza --tree --level=1 --colour=always --icons=always {} } else { bat --color=always --style=numbers --line-range=:500 {} }'
--- Tools/zoxide.ps1
+++ Tools/zoxide.ps1
@@ -1,6 +1,4 @@
-# Escape " with "". As fzf on Windows uses cmd by default
 $env:ZOXIDE_FZF_CUSTOM_PREVIEW = 'eza --tree --level=1 --colour=always --icons=always ({} -replace "[^\t]+\t", "")'
 $env:_ZO_FZF_OPTS = $env:FZF_DEFAULT_OPTS + " --preview='$env:ZOXIDE_FZF_CUSTOM_PREVIEW'"
 
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
-
