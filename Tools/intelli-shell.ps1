25fef54e778a196c6a3049b17260c59d35be795e
EmTee <>
2026-02-08 09:49:58 +1100

    Fixed typo

--- Tools/intelli-shell.ps1
+++ Tools/intelli-shell.ps1
@@ -1,4 +1,4 @@
-$env:INTELLI_HOME = "$env:USERPROFIlE/intelli-shell/data"
+$env:INTELLI_HOME = "$env:USERPROFILE/intelli-shell/data"
 $env:INTELLI_CONFIG = "$env:XDG_CONFIG_HOME/intelli-shell/config.toml"
 $env:INTELLI_COMMANDS = "$env:XDG_CONFIG_HOME/intelli-shell/commands/"
 $env:INTELLI_SEARCH_HOTKEY = "Ctrl+e"
